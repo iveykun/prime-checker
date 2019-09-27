354acfec780be656b1a3cbd915f584a2bb418b00
Kevin Yu <44329173+iveykun@users.noreply.github.com>
2019-09-27 13:47:59 -0400

    Add files via upload

---
+++ testtt.py
@@ -0,0 +1,21 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+
+
+def primes(num):
+    counter = 2
+    while counter < num:
+
+        if num%counter == 0:
+            print(counter,"f")
+                
+            break
+        counter +=1
+        
+    if counter == num:
+        print("t")
+        
+while 1==1:
+    primes(int(input("enter prime")))
